c7015c2d6b4b576752b5523d09a079c855800b32
Idris Shittu <115847693+idrisshittu02@users.noreply.github.com>
2025-09-27 23:49:11 +0100

    Update page.tsx

--- Cartify/app/page.tsx
+++ Cartify/app/page.tsx
@@ -1,14 +1,8 @@
-import Hero from "@/components/Hero";
-
-
-
-const Home = () => {
+export default function HomePage() {
   return (
-    <div className="w-full h-screen">
-      <Hero />
-      <h1>Home</h1>
+    <div>
+      <h1>Welcome to Cartify</h1>
+      <p>Start shopping now!</p>
     </div>
   )
 }
-
-export default Home;
